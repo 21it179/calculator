6ef1902371392be804ff1149022402dd44190505
21it179 <126048582+21it179@users.noreply.github.com>
2023-10-13 12:25:03 +0530

    initial

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
+# Initial Setup
+from PyQt5 import QtCore, QtGui, QtWidgets
+import re
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(338, 395)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("pyshine.bmp"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.mw  = MainWindow
+
+if _name_ == "_main_":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
